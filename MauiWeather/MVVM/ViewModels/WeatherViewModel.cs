f9bd1caa091c3f83f064bdc274f0282ddefc770b
rushi-smb <rushi@smbios.co.in>
2023-03-03 00:06:03 +0530

    Obtaining Weather of Place

--- MauiWeather/MVVM/ViewModels/WeatherViewModel.cs
+++ MauiWeather/MVVM/ViewModels/WeatherViewModel.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using MauiWeather.MVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
@@ -9,13 +11,46 @@ namespace MauiWeather.MVVM.ViewModels
 {
     public class WeatherViewModel
     {
+
+        public WeatherData WeatherData { get; set; }
+
+        private HttpClient client;
+
+        public WeatherViewModel()
+        {
+            client = new HttpClient();
+        }
+
         // Get Coordinates of City search in Search Bar
         public ICommand SearchCommand =>
             new Command(async (searchText) =>
             {
                 var location =
-                await GetCoordinatesAsync(searchText.ToString());
+                    await GetCoordinatesAsync(searchText.ToString());
+                await GetWeather(location);
             });
+
+
+        // Obtaining Weather of Place
+        private async Task GetWeather(Location location)
+        {
+            var url =
+                 $"https://api.open-meteo.com/v1/forecast?latitude={location.Latitude}&longitude={location.Longitude}&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&timezone=America%2FChicago";
+
+            var response =
+              await client.GetAsync(url);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var responseStream = await response.Content.ReadAsStreamAsync())
+                {
+                    var data = await JsonSerializer
+                         .DeserializeAsync<WeatherData>(responseStream);
+                    WeatherData = data;                 
+                }
+            }
+        }
+
 
         private async Task<Location> GetCoordinatesAsync(string address)
         {
